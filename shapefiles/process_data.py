cd939ebb129be9e1aa0a4c0085bf7e6d2353ec48
Felipe Aguirre Martinez <felipeam86@gmail.com>
2021-01-20 19:22:11 +0200

    Clean wrong provinces IDs

--- shapefiles/process_data.py
+++ shapefiles/process_data.py
@@ -13,11 +13,29 @@ def unzip_worldbank_files(worldbank_shapefiles: Path = SHAPEFILES_PATH):
             zipfh.extractall(worldbank_shapefiles / zipfile.stem)
 
 
+def clean_province_ids(df):
+    provinces_ids = (
+        df.groupby("Province")
+            .Prov_ID.value_counts()
+            .rename("count")
+            .sort_values(ascending=False)[:5]
+            .reset_index(-1)
+            .drop(columns=["count"])
+            .Prov_ID.to_dict()
+    )
+    df.loc[:, "Prov_ID"] = df.Province.map(provinces_ids)
+
+    return df
+
+
 def process_worldbank_shapefile(worldbank_shapefiles: Path = SHAPEFILES_PATH):
     worldbank_shapefiles = Path(worldbank_shapefiles)
     for shapefile in worldbank_shapefiles.glob("**/*.shp"):
         df = gpd.read_file(shapefile).to_crs("EPSG:4326")
         df.to_parquet(worldbank_shapefiles / shapefile.with_suffix(".parquet").name)
+        if shapefile.name == "Village.shp":
+            print("cleaning")
+            df = clean_province_ids(df)
 
 
 def main():
